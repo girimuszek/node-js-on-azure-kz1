111e55eb0ef1014866d979c3c71881ae9afdac2f
Kacper Zalewski <112494026+girimuszek@users.noreply.github.com>
2025-02-08 18:29:25 -0600

    added stuff for the game

---
+++ static/diceroll/index.js
@@ -0,0 +1,84 @@
+window.onload = function initiate() {
+
+    let myTurn = false;
+    let herTurn = true;
+    let myHP = 30;
+    document.getElementById("myHPtext").textContent = `${myHP}`;
+    
+    // d20 Die
+    function d20() {
+        return Math.floor(Math.random() * 20) + 1;
+    }
+
+    // d8 Die 
+    function d8() {
+        return Math.floor(Math.random() * 8) + 1;
+    }
+
+    function switchTurns() {
+        myTurn = !myTurn;
+        herTurn = !herTurn;
+        
+
+
+    }
+    //rolls d20 to seewho gets initiative
+    function rollInitiative() {
+        let initiativeRoll = d20();
+
+        //gets the element, and prints out the outcome of initiativeRoll
+        document.getElementById("initiativeResult").textContent = `Initiative Roll: ${initiativeRoll}`;
+        let initiativeResult2message = ""
+        if (initiativeRoll > 10){
+            initiativeResult2message = "You go first!";
+            switchTurns()
+        } else{
+            initiativeResult2message = "Maja goes first!";
+        }
+        
+        document.getElementById("initiativeResult2").textContent = initiativeResult2message;
+        fight()
+    }
+
+
+    function myAttack(){
+        document.getElementById("attackBtn").disabled = false;
+        document.getElementById("combat").textContent = `You can't bring yourself to hurt your cat`;
+        fight();
+    }
+
+
+    function herAttack(){
+        let damage = d8();
+        myHP -= damage
+        document.getElementById("combat").textContent = `Maja deals ${damage} damage!`;
+        document.getElementById("myHPtext").textContent = `${myHP}`;
+        
+        if (myHP <= 0) {
+            document.getElementById("combat").textContent = "You have been defeated!";
+            document.getElementById("attackBtn").disabled = true;
+            return;
+        }
+        switchTurns();
+        fight();
+    }
+
+    function fight(){
+        if (myTurn = false) {
+            herAttack()
+        } else {
+            myAttack()
+        }
+    }
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter" && myTurn) {
+            myAttack();
+        }
+    });
+
+    document.getElementById("rollInitiativeBtn").addEventListener("click", rollInitiative);
+    document.getElementById("attackBtn").addEventListener("click", myAttack);
+    document.getElementById("attackBtn").disabled = true;
+
+};
